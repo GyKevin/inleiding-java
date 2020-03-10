5034c1cddaa79d7fbcf557e9332aeba8cb21ae2f
kvind <kvindso@gmail.com>
2020-03-10 10:44:36 +0100

    h02 opdracht 2

---
+++ src/h02/Opdracht2_2.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht2_2 extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Kevin", 50, 60 );
+
+        g.setColor(Color.red);
+        g.drawString("Gyori", 50, 70 );
+    }
+
+}
